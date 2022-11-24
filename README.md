a9591c3600806a7cd227965f3ae8906fa6657a22
daniil4udo <daniil4udo@gmail.com>
2022-11-24 16:10:17 +0400

    🚀 first aka première aka prima aka أول

---
+++ README.md
@@ -0,0 +1,76 @@
+# @democrance/lint-config
+
+[![npm](https://img.shields.io/npm/v/@democrance/eslint-config?color=a1b858&label=)](https://npmjs.com/package/@democrance/eslint-config)
+
+- Single quotes, no semi
+- Auto fix for formatting (aimed to be used standalone without Prettier)
+- TypeScript, Vue, React out-of-box
+- Lint also for json, yaml, markdown
+- Sorted imports, dangling commas for cleaner commit diff
+- Reasonable defaults, best practices, only one-line of config
+
+## Usage
+
+### Install ESlint
+
+```bash
+pnpm add -D eslint @democrance/eslint-config
+```
+
+### Config `.eslintrc.js`
+
+```cjs
+'use strict'
+
+module.exports = {
+  "extends": "@democrance"
+}
+```
+
+> You don't need `.eslintignore` normally as it has been provided by the preset.
+
+### Add script for package.json
+
+For example:
+
+```json
+{
+  "scripts": {
+    "lint": "eslint .",
+    "lint:fix": "eslint . --fix"
+  }
+}
+```
+
+### Config VS Code auto fix
+
+Create `.vscode/settings.json`
+
+```json
+{
+  "prettier.enable": false,
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true
+  }
+}
+```
+
+### Install Stylelint
+
+```bash
+pnpm add -D stylelint @democrance/stylelint-config
+```
+
+### Config `stylelint.config.js`
+
+```js
+'use strict'
+
+module.exports = {
+	extends: '@democrance/stylelint-config'
+}
+```
+
+## License
+
+[MIT](./LICENSE) License &copy; 2019-PRESENT [Daniil Chumachenko](https://github.com/daniil4udo)
---
+++ packages/eslint/index.js
@@ -0,0 +1,241 @@
+'use strict'
+
+module.exports = {
+	// https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
+	// This option interrupts the configuration hierarchy at this file
+	// Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
+	root: true,
+
+	globals: {
+		// ga: true, // Google Analytics
+		// google: true, // Google Maps
+		process: true,
+	},
+
+	extends: [
+		'@antfu',
+
+		// Import
+		// https://github.com/benmosher/eslint-plugin-import
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
+
+		// 'plugin:putout/recommended',
+	],
+
+	plugins: [
+		'import',
+		'unused-imports',
+		// 'putout',
+	],
+
+	rules: {
+		// Only allow debugger in development
+		'no-debugger': process.env.PRE_COMMIT ? 'error' : 'off',
+
+		// Only allow `console.log` in development
+		'no-console': process.env.PRE_COMMIT ? [ 'error', { allow: [ 'warn', 'error' ] }] : 'off',
+
+		// 🛑 OFF RULES
+		'no-tabs': 'off',
+
+		// 🛠 RULES
+
+		'array-bracket-spacing': [
+			'error',
+			'always',
+			{
+				objectsInArrays: false,
+			},
+		],
+		'eol-last': [
+			'error',
+			'always',
+		],
+		'newline-per-chained-call': [
+			'error',
+			{
+				ignoreChainWithDepth: 3,
+			},
+		],
+		'no-useless-concat': 'error',
+		'prefer-destructuring': [
+			'warn',
+			{
+				array: false,
+				object: true,
+			},
+		],
+		'quotes': [
+			'error',
+			'single',
+			{
+				allowTemplateLiterals: true,
+			},
+		],
+
+		// Indent configuration
+		// 'indent': [
+		//     'off',
+		//     4,
+		//     {
+		//         SwitchCase: 1,
+		//         VariableDeclarator: 1,
+		//         outerIIFEBody: 1,
+		//         flatTernaryExpressions: false,
+		//         offsetTernaryExpressions: false,
+		//         ignoreComments: false,
+		//     },
+		// ],
+		'@typescript-eslint/consistent-type-imports': 'error',
+		'@typescript-eslint/indent': [
+			'error',
+			'tab',
+			{
+				SwitchCase: 1,
+				VariableDeclarator: 1,
+				outerIIFEBody: 1,
+				MemberExpression: 1,
+				FunctionDeclaration: {
+					parameters: 1,
+					body: 1,
+				},
+				FunctionExpression: {
+					parameters: 1,
+					body: 1,
+				},
+				CallExpression: {
+					arguments: 1,
+				},
+				ArrayExpression: 1,
+				ObjectExpression: 1,
+				ImportDeclaration: 1,
+				flatTernaryExpressions: false,
+				ignoreComments: false,
+				ignoredNodes: [
+					'TemplateLiteral *',
+					'JSXElement',
+					'JSXElement > *',
+					'JSXAttribute',
+					'JSXIdentifier',
+					'JSXNamespacedName',
+					'JSXMemberExpression',
+					'JSXSpreadAttribute',
+					'JSXExpressionContainer',
+					'JSXOpeningElement',
+					'JSXClosingElement',
+					'JSXFragment',
+					'JSXOpeningFragment',
+					'JSXClosingFragment',
+					'JSXText',
+					'JSXEmptyExpression',
+					'JSXSpreadChild',
+					'TSTypeParameterInstantiation',
+				],
+				offsetTernaryExpressions: true,
+			},
+		],
+		'vue/html-indent': [
+			'error',
+			'tab',
+			{
+				ignores: [ 'VElement[name=highlight-code].children' ],
+			},
+		],
+
+		// Import
+		'no-duplicate-imports': 'error',
+		'unused-imports/no-unused-imports': 'error',
+		'import/namespace': [
+			'error',
+			{
+				allowComputed: true,
+			},
+		],
+		'import/order': [
+			'error',
+			{
+				'groups': [
+					'builtin',
+					'external',
+					'internal',
+					'parent',
+					'sibling',
+					'index',
+					'unknown',
+				],
+				'newlines-between': 'always',
+				'alphabetize': {
+					order: 'asc',
+				},
+				'pathGroups': [{
+					pattern: '@/**',
+					group: 'internal',
+					position: 'after',
+				}],
+			},
+		],
+
+		// Vue
+		'vue/attributes-order': [
+			'error',
+			{
+				alphabetical: true,
+				order: [
+					'DEFINITION',
+					[
+						'LIST_RENDERING',
+						'CONDITIONALS',
+						'RENDER_MODIFIERS',
+					],
+					[
+						'GLOBAL',
+						'UNIQUE',
+					],
+					'TWO_WAY_BINDING',
+					'OTHER_DIRECTIVES',
+					'OTHER_ATTR',
+					'EVENTS',
+					'CONTENT',
+				],
+			},
+		],
+		'vue/component-tags-order': [
+			'error',
+			{
+				order: [
+					'template',
+					'script[setup]',
+					'script:not([setup])',
+					'style',
+				],
+			},
+		],
+		'vue/html-closing-bracket-newline': [
+			'error',
+			{
+				multiline: 'always',
+				singleline: 'never',
+			},
+		],
+		'vue/html-closing-bracket-spacing': 'error',
+		'vue/max-attributes-per-line': [
+			'error',
+			{
+				multiline: {
+					max: 1,
+				},
+				singleline: {
+					max: 2,
+				},
+			},
+		],
+		'vue/no-template-shadow': 'error',
+		'vue/this-in-template': [
+			'error',
+			'never',
+		],
+	},
+}
+
